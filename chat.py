3f25800e852cf55a3275dbaf73092d70316c2f6f
Tommy <happytommyl@gmail.com>
2022-06-24 02:02:39 +0800

    add docstring

--- chat.py
+++ chat.py
@@ -1,43 +1,49 @@
-import asyncio
+""" connect to bilibili live room and send chat. """
 
+import asyncio
+import configparser
 from prompt_toolkit import PromptSession
 from prompt_toolkit.patch_stdout import patch_stdout
 from bilibili_api import live, Credential, Danmaku
-import configparser
 
 
 def load_conf(path):
+    """return a configparser for the configuration information from given path"""
     conf = configparser.ConfigParser(interpolation=None)
     conf.read(path)
     return conf
 
 
-def load_cred(configParser):
-    cred = configParser.items("Credential")
-    SESSDATA, buvid3, bili_jct = [i[1] for i in cred]
-    cred = Credential(sessdata=SESSDATA, bili_jct=bili_jct, buvid3=buvid3)
-    return cred
+def load_cred(parser):
+    """return the credential infomation to login the chat room. """
+    credential = parser.items("Credential")
+    sessdata, buvid3, bili_jct = [i[1] for i in credential]
+    credential = Credential(sessdata=sessdata, bili_jct=bili_jct, buvid3=buvid3)
+    return credential
 
 
-def load_room(configParser):
-    room = configParser.items("Room")
+def load_room(parser):
+    """return the room id. """
+    room = parser.items("Room")
     return int(room[0][1])
 
 
 def setup(path):
+    """return the configuration information"""
     config = load_conf(path)
-    cred = load_cred(config)
-    room_id = load_room(config)
+    credential = load_cred(config)
+    room = load_room(config)
 
-    return [room_id, cred]
+    return [room, credential]
 
 
 async def live_chat(room_id, cred):
+    """background process to display the live chat"""
     danmaku = live.LiveDanmaku(room_id, credential=cred)
 
     @danmaku.on('DANMU_MSG')
     async def on_danmaku(event):
-        # 收到弹幕
+        """print out the sender and the chat content"""
         print(f"{event['data']['info'][2][1]}: {event['data']['info'][1]}")
 
     try:
@@ -47,6 +53,7 @@ async def live_chat(room_id, cred):
 
 
 async def shell(room_id, cred):
+    """a prompt session where you can type in and send chat."""
     session = PromptSession(">>> ")
     room = live.LiveRoom(room_id, credential=cred)
     while True:
@@ -59,7 +66,7 @@ async def shell(room_id, cred):
 
 
 async def main(room_id, cred):
-    
+    """entry point"""
     with patch_stdout():
         background_task = asyncio.create_task(
             live_chat(room_id=room_id, cred=cred))
