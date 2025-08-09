d5ceace54990d90023ac17909df2cc122eaf779f
happytommyl <happytommyl@gmail.com>
2025-08-10 02:24:39 +0800

    use main.py as entry point

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def main():
-    print("Hello from bilibili-live-chat!")
+    import chat
+    import asyncio
+    room_id, cred = chat.setup('conf.ini')
+    asyncio.run(chat.main(room_id=room_id, cred=cred))
+
 
 
 if __name__ == "__main__":
